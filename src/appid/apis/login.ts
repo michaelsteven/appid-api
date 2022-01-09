2e51c4a49f4f29eb593e7be3620f998d769a8c99
Michael Hepfer <michael.hepfer@ibm.com>
2022-01-09 13:37:04 -0600

    fixes auth error message when calling protected endpoint

--- src/appid/apis/login.ts
+++ src/appid/apis/login.ts
@@ -21,3 +21,23 @@ export const loginWithCredentials = async (username: string, password: string, l
   };
   return awaitFetch(url, options);
 };
+
+export const loginWithRefreshToken = async (refreshToken: string, accessToken?: string, locale: string): Promise<AccessToken> => {
+  const url = `${APPID_SERVICE_ENDPOINT}/oauth/v4/${APPID_API_TENANT_ID}/token`;
+  const base64Creds = Buffer.from(`${APPID_CLIENT_ID}:${APPID_SECRET}`).toString('base64');
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+      appid_access_token: accessToken,
+    }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${base64Creds}`,
+      'Accept-Language': locale,
+    },
+  };
+  return awaitFetch(url, options);
+};
--- src/authentication.ts
+++ src/authentication.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import jwtDecode from 'jwt-decode';
 import { AccessToken } from './appid/models/AccessToken';
+import { ApiError } from './helpers/errors';
+// import { AuthPublicKey } from './helpers/authPublicKey';
 // import { APPID_CLIENT_ID, APPID_API_TENANT_ID } from './helpers/env';
 
 export function expressAuthentication (request: express.Request, securityName: string, scopes?: string[]): Promise<any> {
@@ -9,24 +11,27 @@ export function expressAuthentication (request: express.Request, securityName: s
 
     return new Promise((resolve, reject) => {
       if (!authorization || !authorization.startsWith('Bearer')) {
-        reject(Error('Unauthorized. No authorization header of type Bearer present'));
+        return reject(new ApiError(401, 'Unauthorized. No authorization header of type Bearer present'));
       } else {
         const token = authorization.replace(/Bearer (.*)$/, '$1');
         const decodedToken = jwtDecode(token) as AccessToken;
         // TODO: verify token signature
+        // const authPublicKey: AuthPublicKey = AuthPublicKey.getInstance();
+        // const publicKey = authPublicKey.getKeys();
         // TODO: Verify token tenant
         // TODO: Verify token client id against aud: Array<string>
         // TODO: Verify token is not expired
 
         // verify token contains required scopes
         if (!containsRequiredScopes(decodedToken, scopes || [])) {
-          reject(Error('Insufficient Permissions'));
+          return reject(new ApiError(401, 'Insufficient Permissions'));
+        } else {
+          return resolve(token);
         }
-        resolve(token);
       }
     });
   }
-  return Promise.reject(Error('No supported authentication type matched'));
+  return Promise.reject(new ApiError(401, 'No supported authentication type matched'));
 };
 
 /**
